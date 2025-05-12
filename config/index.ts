32fd961d03c6ea4bec64713c6546d41e425b57fc
d-wormhole <526441447@qq.com>
2025-05-12 21:36:20 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -7,7 +7,7 @@ export const APP_INFO: AppInfo = {
   description: '',
   copyright: '',
   privacy_policy: '',
-  default_language: 'cn-Hans',
+  default_language: 'zh-Hans',
 }
 
 export const isShowPrompt = false
